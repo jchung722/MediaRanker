fcb1d7c54cec132f75d1066353bfc166c076349e
Jessica <jchung722@gmail.com>
2016-10-12 13:40:36 -0700

    presence valiadtion added

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,5 @@
 class Album < ActiveRecord::Base
+  validates :name, presence: true
+  validates :artist, presence: true
+  validates :description, presence: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+  validates :name, presence: true
+  validates :director, presence: true
+  validates :description, presence: true
 end
