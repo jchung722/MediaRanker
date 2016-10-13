cc3706ad3ce3abc81fed2f02a2e69f438c35ea1c
Jessica <jchung722@gmail.com>
2016-10-13 09:57:36 -0700

    must have name test

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,13 +1,14 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  test "the truth" do
+    assert true
+  end
 
-test "An album must have a name" do
-  assert albums(:X).valid?
-  albums(:X).name = nil
-  assert_not albums(:X).valid?
+  test "An album must have a name" do
+    assert albums(:X).valid?
+    albums(:X).name = nil
+    assert_not albums(:X).valid?
+  end
+
 end
